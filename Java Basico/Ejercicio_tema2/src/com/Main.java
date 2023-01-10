e97aedfbce4a6c74a059957b7050664abe227d09
xaviergbdev <121193131+xaviergbdev@users.noreply.github.com>
2023-01-10 11:57:16 -0500

    Update Main.java

--- Java Basico/Ejercicio_tema2/src/com/Main.java
+++ Java Basico/Ejercicio_tema2/src/com/Main.java
@@ -6,41 +6,41 @@ public class Main {
         //Enteros
 
         byte numero1 = 1;
-        System.out.println("Numero tipo byte" + numero1);
+        System.out.println("Numero tipo byte " + numero1);
 
         short numero2 = 2;
-        System.out.println("numero tipo short" + numero2);
+        System.out.println("numero tipo short " + numero2);
 
         int numero3 = 3;
-        System.out.println("Numero tipo int" + numero3);
+        System.out.println("Numero tipo int " + numero3);
 
         long numero4 = 4;
-        System.out.println("Numero tipo long" + numero4);
+        System.out.println("Numero tipo long " + numero4);
 
         //numero con punto flotante
 
         float numero5 = 5.0f;
-        System.out.println("Numero con punto flotante" + numero5);
+        System.out.println("Numero con punto flotante " + numero5);
 
         double numero6 = 6.0;
-        System.out.println("Numero con punto flotante" + numero6);
+        System.out.println("Numero con punto flotante " + numero6);
 
         // Tipos booleans
 
         boolean b1 = true;
-        System.out.println("Tipo con booleans" + b1);
+        System.out.println("Tipo con booleans " + b1);
         boolean b2 = false;
-        System.out.println("tipo con booleans" + b2);
+        System.out.println("tipo con booleans " + b2);
 
         //Tipo caracteres
 
         char ca1 = 'a';
-        System.out.println("tipo caracteres" + ca1);
+        System.out.println("tipo caracteres " + ca1);
 
         //Tipo cadena de caracteres
 
         String ca2 = "Hola Mundo";
-        System.out.println("tipo cadena de caracteres" + ca2);
+        System.out.println("tipo cadena de caracteres " + ca2);
 
 
 
