3add8fb75f26d3372e90419e96d07e2cfd953c6f
xaviergbdev <121193131+xaviergbdev@users.noreply.github.com>
2023-02-24 16:42:00 -0500

    Add files via upload

---
+++ Curso C#/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            Console.WriteLine("Ingrese su nombre ? ");
+            name = Console.ReadLine();
+            Console.WriteLine("EL nombre ingresado es  - > "+ name);
+
+        }
+    }
+}
