47b3e8d52a960015d30002b1ed70bc2084ff9b65
xaviergbdev <121193131+xaviergbdev@users.noreply.github.com>
2023-02-24 16:54:59 -0500

    Add files via upload

---
+++ Curso C#/Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio que muetra la hora actual
+            Console.WriteLine("La hora actual es : ");
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+            
+        }
+    }
+}
